dfb3ea0d62b9041dbfdbb3654de17e4e95bf2e55
dbbrowne <duncan.b.browne@gmail.com>
2022-05-17 22:31:21 +0100

    tests(pokemon handler): extend isComplete

--- __tests__/handlers/pokemon.js
+++ __tests__/handlers/pokemon.js
@@ -1,5 +1,6 @@
 import cache from '../../src/db/cache.js'
 import pokemonHandler from '../../src/handlers/pokemon.js'
+import { isCompletePokemon } from '../../src/handlers/pokemon.js'
 
 const venusaur = {
   'id': 3,
@@ -8,11 +9,12 @@ const venusaur = {
   'description': 'The plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
   'isLegendary': false,
 }
+const venusaurCacheIndex = venusaur.id - 1
 
 describe('pokemon Handler', ()=>{
   it('gets pokemon from cache if cache data is complete', async ()=>{
     cache.init()
-    global.pokeCache[venusaur.id - 1] = venusaur
+    global.pokeCache[venusaurCacheIndex] = venusaur
 
     const venusaurFromCache = await pokemonHandler.byName(venusaur.name)
 
@@ -20,3 +22,16 @@ describe('pokemon Handler', ()=>{
   })
 })
 
+describe('isCompletePokemon', ()=>{
+  it('returns true if cached data is complete',()=>{
+    expect(isCompletePokemon(venusaur)).toBe(true)
+  })
+  it('returns false if cached data is not complete',()=>{
+    const incompletePokemon = { ...venusaur, isLegendary: null }
+    expect(isCompletePokemon(incompletePokemon)).toBe(false)
+  })
+  it('returns false if cached data is not valid',()=>{
+    const incompletePokemon = { ...venusaur, url: Number(1) }
+    expect(isCompletePokemon(incompletePokemon)).toBe(false)
+  })
+})
--- src/handlers/pokemon.js
+++ src/handlers/pokemon.js
@@ -1,9 +1,30 @@
 import cache from '../db/cache.js'
 
+class Pokemon {
+  constructor(
+    id = 0,
+    name = '',
+    url = '',
+    description = '',
+    isLegendary = false
+  ){
+    this.id = id
+    this.name = name
+    this.url = url
+    this.description = description
+    this.isLegendary = isLegendary
+  }
+}
+
+const blankPokemon = new Pokemon()
+export function isCompletePokemon(pokemon){
+  return Object.keys(blankPokemon).every( prop => {
+    return typeof blankPokemon[prop] === typeof pokemon[prop]
+  })
+}
+
 async function getPokemonDataByName(name){
   const fromCache = cache.getByName(name)
-
-
 
   return fromCache
 }
