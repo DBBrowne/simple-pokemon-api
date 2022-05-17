ef2740e97d78f2df3caf80b82b7ddf1c552d1dd1
dbbrowne <duncan.b.browne@gmail.com>
2022-05-17 22:10:46 +0100

    cache: getcached data

--- __tests__/db/cacheFromGlobal.js
+++ __tests__/db/cacheFromGlobal.js
@@ -1,4 +1,6 @@
+import cache from '../../src/db/cache.js'
 import { server } from '../../src/index.js'
+
 
 afterAll(async () => {
   await global.tearDown({ server })
@@ -9,9 +11,19 @@ const firstPokemon = {
   'name': 'bulbasaur',
   'url': 'https://pokeapi.co/api/v2/pokemon/1/',
 }
+const charmander = {
+  id: 4,
+  name: 'charmander',
+  url: 'https://pokeapi.co/api/v2/pokemon/4/',
+}
 
 describe('globalCache', ()=>{
   it('attaches at server start', ()=>{
     expect(global.pokeCache[0]).toMatchObject(firstPokemon)
+  })
+  it('gets from cache',()=>{
+    const charmanderFromCache = cache.getByName(charmander.name)
+
+    expect(charmanderFromCache).toMatchObject(charmander)
   })
 })
