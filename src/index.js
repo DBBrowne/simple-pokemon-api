371153159969a01c2e5dfc701f730156a86ae654
dbbrowne <duncan.b.browne@gmail.com>
2022-05-18 11:29:45 +0100

    refactor: logging

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import Http from 'http'
 import express from 'express'
 
 import cache from './db/cache.js'
-import logDev from './common/logDev.js'
+import { logDebug, reqLogger } from './common/loggers.js'
 import { port } from './config/environment.js'
 import router from './config/router.js'
 
 export let server
 
 const app = express.Router()
+app.use('/', reqLogger)
 app.use('/api', router)
 
 const expressServer = express()
@@ -19,7 +20,7 @@ async function startServer(){
 
   server = Http.createServer(expressServer)
   server.listen(port, ()=>{
-    logDev('Up and running on', server.address())
+    logDebug('Up and running on', server.address())
   })
 }
 
