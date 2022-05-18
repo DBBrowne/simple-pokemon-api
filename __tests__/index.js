a04f0fce52d99176463f9668a1f88c8a7fee82e5
dbbrowne <duncan.b.browne@gmail.com>
2022-05-18 12:16:12 +0100

    test: 404 on bad route

--- __tests__/index.js
+++ __tests__/index.js
@@ -6,9 +6,13 @@ afterAll(async () => {
   await global.tearDown({ server })
 })
 
-describe('API root path', ()=>{
+describe('API', ()=>{
   it('responds to root', async ()=>{
     const response = await request(server).get('/api/')
     expect(response.statusCode).toBe(200)
+  })
+  it('returns 404 if route does not exist', async()=>{
+    const response = await request(server).get('/api/elephants')
+    expect(response.statusCode).toBe(404)
   })
 })
