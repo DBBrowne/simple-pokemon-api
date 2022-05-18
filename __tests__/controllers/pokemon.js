8d851e93b612575118090e9f8e1e73f439ab03f9
dbbrowne <duncan.b.browne@gmail.com>
2022-05-18 12:57:48 +0100

    tests: refactor handler tests

--- __tests__/controllers/pokemon.js
+++ __tests__/controllers/pokemon.js
@@ -1,28 +1,39 @@
 import request from 'supertest'
 
+import { isCompletePokemon } from '../../src/handlers/pokemon.js'
 import pokeApi from '../../src/external/pokeApi'
-
 import { server } from '../../src/index.js'
+
+beforeAll(()=>{
+  pokeApi.getDetails = jest.fn(()=>uncachedPokemon)
+})
+beforeEach(()=>{
+  jest.clearAllMocks()
+})
 
 afterAll(async () => {
   await global.tearDown({ server })
 })
 
-const charizard = { 
+
+const uncachedPokemon = { 
   'name': 'charizard',
   'description': 'Spits fire that is hot enough to melt boulders. Known to cause forest fires unintentionally',
   'is_legendary': false,
 }
 
 describe('Poke API', ()=>{
-  it('gets Charizard from the external pokeAPI', async ()=>{
-    pokeApi.getDetails = jest.fn(()=>charizard)
+  it('gets pokemon from the external pokeAPI if cached data is incomplete', async ()=>{
+    const cacheIndex = global.pokeCacheIndex[uncachedPokemon.name]
+    global.pokeCache[cacheIndex] = {}
+    expect(isCompletePokemon(global.pokeCache[cacheIndex])).toBe(false)
     
-    const response = await request(server).get(`/api/pokemon/${charizard.name}`)
+    const response = await request(server).get(`/api/pokemon/${uncachedPokemon.name}`)
     
-    expect(pokeApi.getDetails).toBeCalledWith(charizard.name)
+    expect(pokeApi.getDetails).toBeCalledWith(uncachedPokemon.name)
     expect(pokeApi.getDetails).toBeCalledTimes(1)
     expect(response.statusCode).toBe(200)
-    expect(response.body).toMatchObject(charizard)
+    expect(response.body).toMatchObject(uncachedPokemon)
   })
+
 })
