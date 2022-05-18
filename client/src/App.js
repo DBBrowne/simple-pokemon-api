3361dc1f23681f1316f542b11259941471384224
dbbrowne <duncan.b.browne@gmail.com>
2022-05-18 22:52:44 +0100

    feat(FE): search error handling

--- client/src/App.js
+++ client/src/App.js
@@ -15,29 +15,34 @@ const initPokeData = {
 export default function App() {
   let [pokeData, setPokeData] = React.useState(initPokeData)
   let [searchEntry, setSearchEntry] = React.useState('')
+  let [isError, setIsError] = React.useState(false)
 
   const getPokeData = (name)=>{
     getPokemonData(name).then(res=>{
       setPokeData(res.data)
+    }).catch(err=>{
+      console.log(err)
+      setIsError(true)
     })
   }
-  React.useEffect(()=>{
-    getPokeData('charmander')
-  }, [])
-
+  
   const handleSearchChange = (e) =>{
     setSearchEntry(e.target.value)
   }
   const handleSearchSubmit = (e) => {
     e.preventDefault()
-
+    
     getPokeData(searchEntry)
   }
-
+  
+  React.useEffect(()=>{
+    getPokeData('charmander')
+  }, [])
+  
   return (
     <section className="main">
-      <SearchBar onSubmit={handleSearchSubmit} onChange={handleSearchChange}/>
-      <PokeInfo {...pokeData}/>
+      <SearchBar onSubmit={handleSearchSubmit} onChange={handleSearchChange} />
+      <PokeInfo pokeData={pokeData} isError={isError}/>
     </section>
   );
 }
--- client/src/containers/PokeInfo/PokeInfo.js
+++ client/src/containers/PokeInfo/PokeInfo.js
@@ -1,6 +1,6 @@
 import './PokeInfo.css'
 
-export default function PokeInfo(pokeData){
+export default function PokeInfo({pokeData, isError}){
   const pokeId = String(pokeData.id).padStart(4, '0')
   let pokeName = pokeData.name
   pokeName = pokeName[0].toUpperCase() + pokeName.slice(1).toLowerCase()
@@ -11,6 +11,9 @@ export default function PokeInfo(pokeData){
         (pokeData.isLegendary ? ' legendary' : '')
       }
     >
+      <small>
+      {isError ? ('We couldn\'t find that one.  How about:') : ''}
+      </small>
       <h3>{pokeId}: {pokeName}</h3>
       <p>{pokeData.description}</p>
     </aside>
