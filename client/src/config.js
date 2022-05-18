0a3f4043954cb0870a6f54c63d5b756ff0302669
dbbrowne <duncan.b.browne@gmail.com>
2022-05-18 16:22:15 +0100

    fix(deploy): env var name and vestigal html

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export const environment = process.env.NODE_ENV
 
 const apiDevUrl = 'http://localhost:4000/api'
-const apiProdUrl = process.env.REACT_APP_PROD_URL
+const apiProdUrl = process.env.REACT_APP_PROD_API_URL
 export const apiBaseUrl = environment === 'production' ? apiProdUrl : apiDevUrl
