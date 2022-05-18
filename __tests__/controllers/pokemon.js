34e8a83abde3f8772a70a14b5a70d8f6bcda28a5
dbbrowne <duncan.b.browne@gmail.com>
2022-05-18 12:58:27 +0100

    tests: cached data is returned if possible

--- __tests__/controllers/pokemon.js
+++ __tests__/controllers/pokemon.js
@@ -22,6 +22,13 @@ const uncachedPokemon = {
   'is_legendary': false,
 }
 
+const cachedPokemon = {
+  'id': 3,
+  'name': 'venusaur',
+  'description': 'The plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
+  'isLegendary': false,
+}
+
 describe('Poke API', ()=>{
   it('gets pokemon from the external pokeAPI if cached data is incomplete', async ()=>{
     const cacheIndex = global.pokeCacheIndex[uncachedPokemon.name]
@@ -35,5 +42,16 @@ describe('Poke API', ()=>{
     expect(response.statusCode).toBe(200)
     expect(response.body).toMatchObject(uncachedPokemon)
   })
-
+  it('gets pokemon from cache', async ()=>{
+    const cacheIndex = global.pokeCacheIndex[cachedPokemon.name]
+    global.pokeCache[cacheIndex] = cachedPokemon
+    expect(isCompletePokemon(cachedPokemon)).toBe(true)
+    expect(isCompletePokemon(global.pokeCache[cacheIndex])).toBe(true)
+    
+    const response = await request(server).get(`/api/pokemon/${cachedPokemon.name}`)
+    
+    expect(pokeApi.getDetails).toBeCalledTimes(0)
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toMatchObject(cachedPokemon)
+  })
 })
