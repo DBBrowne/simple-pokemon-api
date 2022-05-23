3a2cb302afe05a53186509c001c8fc0940f62e8d
dbbrowne <duncan.b.browne@gmail.com>
2022-05-23 12:19:10 +0100

    fix(deploy - aws): renew AWS credentials on script run

--- deploy/aws/deploy.sh
+++ deploy/aws/deploy.sh
@@ -1,3 +1,4 @@
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 029929660699.dkr.ecr.eu-west-2.amazonaws.com
 docker compose -f ./docker-compose.prod.yml up --build --no-start
 docker push 029929660699.dkr.ecr.eu-west-2.amazonaws.com/pokemon-front
 docker push 029929660699.dkr.ecr.eu-west-2.amazonaws.com/pokemon-back
